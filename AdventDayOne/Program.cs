aee1775fa814a9e600bf33a3f50c1121b72f8bfa
Laurent Seguin <4620938+NightFaust@users.noreply.github.com>
2022-12-09 21:45:04 +0100

    Add project files.

---
+++ AdventDayOne/Program.cs
@@ -0,0 +1,18 @@
+﻿string data = File.ReadAllText($"{Environment.CurrentDirectory}/data.txt");
+string[] removedEmptyLines = data.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+List<int> results = new();
+
+foreach (var entry in removedEmptyLines)
+{
+    string[] lines = entry.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+    int calories = 0;
+    for (int i = 0; i < lines.Length; i++)
+        calories += int.Parse(lines[i]);
+
+    results.Add(calories);
+}
+
+// Part 1
+Console.WriteLine($"Result 1: {results.Max()}");
+// Part 2
+Console.WriteLine($"Result 2: {results.OrderByDescending(x => x).Take(3).Sum()}");
