2ac53b77091652505d1f2527dd5db1c31095c104
Laurent Seguin <4620938+NightFaust@users.noreply.github.com>
2022-12-11 18:19:58 +0100

    Advent Day 3 Part One

---
+++ AdventDayThree/Program.cs
@@ -0,0 +1,24 @@
+﻿string data = File.ReadAllText($"{Environment.CurrentDirectory}/data.txt");
+string[] lines = data.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+int result = 0;
+
+foreach (string line in lines)
+{
+    string firstHalf = line[0..(line.Length / 2)];
+    string secondHalf = line[(line.Length / 2)..];
+
+    for (int i = 0; i < firstHalf.Length; i++)
+    {
+        if (secondHalf.Contains(firstHalf[i]))
+        {
+            result += char.IsUpper(firstHalf[i])
+                ? firstHalf[i] - 38
+                : char.ToUpper(firstHalf[i]) - 64;
+
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"Result 1 : {result}");
