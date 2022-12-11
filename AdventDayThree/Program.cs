de788ec7b23c1b26f5cdf5caccb47756e2f4939d
Laurent Seguin <4620938+NightFaust@users.noreply.github.com>
2022-12-11 21:05:45 +0100

    Advent Day 3 part two

--- AdventDayThree/Program.cs
+++ AdventDayThree/Program.cs
@@ -12,13 +12,34 @@ foreach (string line in lines)
     {
         if (secondHalf.Contains(firstHalf[i]))
         {
-            result += char.IsUpper(firstHalf[i])
-                ? firstHalf[i] - 38
-                : char.ToUpper(firstHalf[i]) - 64;
-
+            result += GetPriority(firstHalf[i]);
             break;
         }
     }
 }
 
 Console.WriteLine($"Result 1 : {result}");
+
+// Part 2
+List<string[]> groups = new();
+int newResult = 0;
+
+for (int i = 0; i < lines.Length; i += 3)
+    groups.Add(lines.Skip(i).Take(3).ToArray());
+
+foreach (string[] group in groups)
+{
+    for (int i = 0; i < group[0].Length; i++)
+    {
+        if (group[1].Contains(group[0][i]) && group[2].Contains(group[0][i]))
+        {
+            newResult += GetPriority(group[0][i]);
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"Result 2 : {newResult}");
+
+int GetPriority(char c) =>
+    char.IsUpper(c) ? c - 38 : char.ToUpper(c) - 64;
